854873c9b13f1c30304d8b04e5732d2afaf733b3
Ryan Taylor <ryanmt@byu.net>
2011-06-24 17:21:43 -0600

    working, just want to clean it up later

--- lib/spectral_summing.rb
+++ lib/spectral_summing.rb
@@ -3,9 +3,10 @@ Spectrum = Struct.new(:spectrum, :scan_num, :scan_time, :scan_range, :precursor_
 class Parser
 	attr_accessor :spectra
 	def initialize(file)
-		parse(file)
+		@file = file
 	end
-	def parse(file)
+	def parse(file = nil)
+		file ||= @file
 		require 'ms/msrun'
 		@spectra = []
 		Ms::Msrun.open(file) do |ms|
@@ -14,39 +15,57 @@ class Parser
 			end
 		end
 	end
+	def parse_by_scan_num(scan_nums, file = nil)
+		file ||= @file
+		require 'ms/msrun'
+		@spectra = []
+		Ms::Msrun.open(file) do |ms|
+			ms.each(:ms_level => 2) do |scan|
+				if scan_nums.include?(scan.num)
+					@spectra << Spectrum.new(scan, scan.num, scan.time, (scan.start_mz..scan.end_mz), scan.precursor.mz, scan.precursor.charge_states, scan.spectrum.intensities, scan.spectrum.mzs)
+				end
+			end
+		end
+	end
 end
 
 class Combiner 
 	Defaults = {:bin_window => 0.1, :window_size => 4, :precursor_mass_tolerance_in_ppm =>	10, :tolerant => false}
 	attr_accessor :output_spectra
-	def initialize(spectra, opts = {})
+	def initialize(spectra = nil, opts = {})
 		@spectra = spectra
 		@opts = Defaults.merge(opts)
 	end
 	def combine(spectrum1, spectrum2)
 		tolerance = calculate_daltons_from_ppm(spectrum1.precursor_mass, @opts[:precursor_mass_tolerance_in_ppm] )
 		if tolerance.include?(spectrum2.precursor_mass) or @opts[:tolerant]
-			bin(spectrum1.mz_values, spectrum1.intensities, spectrum2.mz_values, spectrum2.intensities)
+			data_arr = summer(spectrum1.mz_values, spectrum1.intensities, spectrum2.mz_values, spectrum2.intensities)
 		end
+		data_arr
 	end
-	def bin(x1,y1,x2,y2) # What should this return?
+	def summer(x1,y1,x2,y2) # What should this return?
 		endpoints = (x1+x2).each.minmax
 		bin_width = @opts[:bin_window]
 		num_bins = ((endpoints.last - endpoints.first)/bin_width).ceil
 		data_x = [endpoints.first+bin_width/2.0]
 		data_y = Array.new(num_bins, 0)
-		j, k = 0,0; one = [x1,y1]; two = [x2,y2]
-		data_y[0] = ( x1.first == endpoints.first ? y1.first : y2.first )
-		(1..num_bins).each do |i|
-			data_x[i] = data_x[i-1] + bin_width
+		j, k = 0,0
+		one = [x1,y1]; two = [x2,y2]
+		if x1.first == endpoints.first
+			data_x[0] = x1.first
+			data_y[0] += y1.first
+			y1[0] = 0
+		elsif x2.first == endpoints.first
+			data_x[0] = x2.first
+			data_y[0] += y2.first
+			y2[0] = 0
+		end
+		(1..num_bins-1).each do |i|
+			data_x[i] = data_x[i-1] + bin_width 
 			check = data_x[i] + bin_width/2.0
-			puts "check= #{check}"
+			#puts "check= #{check}"
 			if one.first[j]
 				while one.first[j] < check 
-					puts 'while loop 1 init'
-					p one.first[j]
-					p one.last[j]
-
 					data_y[i] += one.last[j]
 					j += 1
 					break if one.first[j].nil?
@@ -54,9 +73,6 @@ class Combiner
 			end
 			if two.first[k]
 				while two.first[k] < check
-					puts 'while loop 2 init'
-					p two.first[k] 
-					p two.last[k]
 					data_y[i] += two.last[k]
 					k += 1
 					break if two.first[k].nil?
@@ -65,18 +81,77 @@ class Combiner
 		end
 		[data_x, data_y]
 	end
+	def combine_for_more_combining(spectrum1, spectrum2)
+		arr = combine(spectrum1, spectrum2)
+		joined_spectrum = Spectrum.new()
+		joined_spectrum.precursor_mass = (spectrum1.precursor_mass + spectrum2.precursor_mass)/2.0
+		joined_spectrum.mz_values = arr.first
+		joined_spectrum.intensities = arr.last
+# Spectrum = Struct.new(:spectrum, :scan_num, :scan_time, :scan_range, :precursor_mass, :charge_states, :intensities, :mz_values)
+		joined_spectrum
+	end
 	def calculate_daltons_from_ppm(mass, ppm)
 		diff = ppm*mass/1e6
 		(mass-diff)..(mass+diff)
 	end
+	def to_mgf(spectrum, filename)
+		File.open(filename,'w') do |out|
+			out.puts "BEGIN IONS"
+			out.puts "TITLE=Spec1:#{spectrum.precursor_mass}_#{spectrum.charge_states.first}"
+			out.puts "CHARGE=#{spectrum.charge_states.to_s}+"
+			# our current mzML parser doesn't have scan.time implemented...
+			spectrum.mz_values.each_with_index do |mz, i|
+				intensity = spectrum.intensities[i]
+				out.puts "#{"%.5f" % mz}/t#{"%.5f" % intensity}" unless intensity == 0
+			end
+			out.puts "END IONS"
+		end
+	end
+	def combine_to_mgf(spectrum1, spectrum2, filename)   # Thanks JOHN!!! Ms-Msrun 0.3.6
+		results = combine(spectrum1, spectrum2)
+		File.open(filename, 'w') do |out|
+			out.puts "BEGIN IONS"
+			out.puts "TITLE=Spec1:#{spectrum1.precursor_mass}_Spec2:#{spectrum2.precursor_mass}.#{spectrum1.scan_num}_#{spectrum2.scan_num}_#{spectrum1.charge_states.first}"
+			out.puts "CHARGE=#{spectrum1.charge_states.to_s}+"
+			# our current mzML parser doesn't have scan.time implemented...
+			results.first.each_with_index do |mz, i|
+				intensity = results.last[i]
+				out.puts "#{"%.5f" % mz}/t#{"%.5f" % intensity}" unless intensity == 0
+			end
+			out.puts "END IONS"
+		end
+	end
 end
 
-=begin
-x1 = Array.new(2,1)
-y1 = Array.new(2,2)
-x2 = Array.new(2,3)
-y2 = Array.new(2,4)
-a = Combiner.new('hello')
-p a.bin(x1,y1,x2,y2)
+if ARGV.size == 0 or ARGV.size % 2 != 0
+	puts "Usage: #{__FILE__} input_file.mzXML scan_nums.txt input_file2.mzXML scan_nums2.txt ... "
+	puts "NOTE: scan_nums.txt files must have a new line break between each integer value."
+	puts 'Returns input_file_input_file2_..._input_file(n).mgf'
+	exit
+else
+	mzXMLs = []
+	scan_nums = []
+	while ARGV.size > 0
+		mzXMLs << ARGV.shift
+		scan_nums << ARGV.shift
+	end
+	scan_nums.map {|file|	IO.readlines(file) }
+# Parse the files and put the data into spectra objects, held within the list of all spectra to combine.
+	spectras = []
+	mzXML.each_with_index do |file, i|
+		@parse_object = Parser.new(file)
+		@parse_object.parse_by_scan_num(scan_nums[i])
+		spectras << @parse_object.spectra
+	end
+	combined_spectrum = spectras.shift
+	combiner = Combiner.new(combined_spectrum)
+	spectras.each do |spectrum|
+		combined_spectrum = combiner.combine_for_more_combining(combined_spectrum, spectrum)
+	end
+	combiner.to_mgf(combined_spectrum, 'combined_multiple_files.mgf')
+end
 
-=end
+		
+
+
+	
--- spec/spectral_summing_spec.rb
+++ spec/spectral_summing_spec.rb
@@ -6,6 +6,8 @@ describe "Parser" do
 		@result = Parser.new(@test)
 		@result.spectra.class.should.equal Array
 	end
+	it 'also can only catch the spectra objects that correlate with a given scan number' do 
+	end
 end
 =begin
 describe 'Comparer' do 
@@ -27,18 +29,30 @@ describe "Combiner" do
 		@y1 = [2.614011764526367, 19.853052139282227, 5.9615044593811035, 6.357842445373535, 5.079195022583008, 2.2274322509765625, 8.529345512390137, 22.411785125732422, 1.846116065979004, 2.229602575302124]
 		@x2 = [187.24366760253906, 191.0662384033203, 195.14126586914062, 197.20376586914062, 199.3022918701172, 200.21475219726562, 201.06857299804688, 202.2320556640625, 203.39401245117188, 205.27679443359375, 207.0498809814453, 208.42347717285156]
 		@y2 = [100000, 1.4693498611450195, 4.156303882598877, 39.14124298095703, 16.008852005004883, 5.559536457061768, 1.8495380878448486, 2.0385212898254395, 2.418201208114624, 1.8486547470092773, 7.051799297332764, 2.423830032348633]
+		@combiner = Combiner.new 'hello'
+		@c = @combiner.summer(@x1,@y1,@x2,@y2)
 	end
-	it 'bins the spectra correctly' do 
-		a = Combiner.new 'hello'
-		c = a.bin(@x1,@y1,@x2,@y2)
-		x = c.first
-		y = c.last
-		(0..c.first.length).each do |i|
-			puts "#{x[i]}, #{y[i]}"
+	it 'bins and sums the spectra correctly' do 
+#		x = @c.first
+#		y = @c.last
+#		(0..@c.first.length-1).each do |i|
+#			puts "#{x[i]}, #{y[i]}"
+#		#	puts "#{x[i+1]}, #{y[i+1]}"
+#		#	puts "#{x[i+2]}, #{y[i+2]}"
+#		end
+		@c.last[202].should.equal 100002.2296025753
+	end
+#  it 'aligns the two spectras' do 
+#	end
+#	it 'sums the intensity values and produces a combined spectra' do 
+#	end
+	it 'generates an mgf file' do 
+		test = TESTFILES + '/test.mzXML'
+		@result = Parser.new(test)
+		@result.spectra.each_with_index do |a, i|
+			puts a.precursor_mass
+			puts i
 		end
-	end
-  it 'aligns the two spectras' do 
-	end
-	it 'sums the intensity values and produces a combined spectra' do 
+			@combiner.combine_to_mgf(@result.spectra[192], @result.spectra[194], 'testing.mgf')
 	end
 end
